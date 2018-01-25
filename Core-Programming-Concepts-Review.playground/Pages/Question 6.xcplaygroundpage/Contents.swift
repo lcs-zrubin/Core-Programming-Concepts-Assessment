3ee1d3679b8d7e30eac75b2df75f6deecbca7a63
Russell Gordon <rgordon@lcs-mba2410.lcs.on.ca>
2018-01-25 15:01:17 -0500

    Question 6

--- Core-Programming-Concepts-Review.playground/Pages/Question 6.xcplaygroundpage/Contents.swift
+++ Core-Programming-Concepts-Review.playground/Pages/Question 6.xcplaygroundpage/Contents.swift
@@ -1,20 +1,33 @@
 /*:
+ [Previous](@previous)
+ ### Learning Goals
+ 
+ * Programming: Goal 5
+    * *Knowledge*
+        * I can write concise statements using Boolean operators or pattern matching to check for multiple conditions.
+ 
+ * Programming: Goal 6
+    * *Knowledge*
+        * I can use comparison operators and arithmetic operators and understand operator precedence (order of operations).
+
  # Question 6
 
- Write an `if-else if-else` statement that will print different statements based on what range `currentHeartRate` falls into.
+ **Write an `if-else if-else` statement that will print different statements based on what range `currentGrade` falls into.**
  
  Below is a list of ranges and the associated statements
  
- - 100-120: "You are in the Very Light zone. Activity in this zone helps with recovery."
- - 121-140: "You are in the Light zone. Activity in this zone helps improve basice endurance and fat burning."
- - 141-160: "You are in the Moderate zone. Activity in this zone helps improve aerobic fitness."
- - 161-180: "You are in the Hard zone. Activity in this zone increases maximum performance capacity for shorter sessions."
- - 181-200: "You are in the Maximum zone. Activity in this zone helps fit athletes develop speed."
- 
- If `currentHeartRate` is above the listed zones, print some kind of warning asking the user to slow down.
+ - 0-49: "Credit is at risk"
+ - 50-59: "Level 1"
+ - 60-69: "Level 2"
+ - 70-79: "Level 3 - Meeting Expectations"
+ - 80-100: "Level 4 - Exceeding Expectations"
+
+ - note:
+    You'll need to adjust the value of `currentGrade` below to test your code thoroughly.
+
  */
 
-let currentHeartRate = 128
+let currentGrade = 75
 
 // Answer question 6 below
 
@@ -28,13 +41,13 @@ let currentHeartRate = 128
  [al]:
  https://www.youtube.com/watch?v=Wa3Wl3T25jo&list=PLTGGOQnktyWs9TlNJ30pgYgypvIGrt3Lx&index=1
  
- ### Learning Goals - Programming
+ ### Learning Goals
  
- * Goal 5
+ * Programming: Goal 5
     * *Knowledge*
         * I can write concise statements using Boolean operators or pattern matching to check for multiple conditions.
  
- * Goal 6
+ * Programming: Goal 6
     * *Knowledge*
         * I can use comparison operators and arithmetic operators and understand operator precedence (order of operations).
  */
