f9be8dbf7ea1609e7c4ce6269aac6ace4400c052
Rubin Zach <zrubin@lcs-mba3136.lcs.on.ca>
2018-01-26 09:25:28 -0500

    Question 6

--- Core-Programming-Concepts-Assessment.playground/Pages/Question 6.xcplaygroundpage/Contents.swift
+++ Core-Programming-Concepts-Assessment.playground/Pages/Question 6.xcplaygroundpage/Contents.swift
@@ -27,10 +27,21 @@
 
  */
 
-let currentGrade = 75
+let currentGrade = 99
 
 // Answer question 6 below
 
+if currentGrade >= 0 && currentGrade <= 49 {
+    print("Credit is at risk")
+} else if currentGrade >= 50 && currentGrade <= 59 {
+    print("Level 1")
+} else if currentGrade >= 60 && currentGrade <= 69 {
+    print("Level 2")
+} else if currentGrade >= 70 && currentGrade <= 79 {
+    print("Level 3 - Meeting Expectations")
+} else if currentGrade >= 80 && currentGrade <= 100 {
+    print("Level 4 - Exceeding Expectations")
+}
 
 /*:
  ## Now share your understanding
